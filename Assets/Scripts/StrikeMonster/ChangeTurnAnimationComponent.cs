19d882c083ed9fa35b95379d09b9c87f06d199de
hongkai.chen <hongkai.chen@seedsudio.com>
2016-04-29 19:05:32 +0800

    * text run animation

---
+++ Assets/Scripts/StrikeMonster/ChangeTurnAnimationComponent.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace StrikeMonster
+{
+
+    public class ChangeTurnAnimationComponent : MonoBehaviour {
+
+        public string ShowText = "1"; 
+        public GameObject PrefabText;
+
+    	// Use this for initialization
+    	void Start () {
+    	
+    	}
+    	
+    	// Update is called once per frame
+    	void Update () {
+    	
+    	}
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ Assets/Scripts/StrikeMonster/RunTextComponent.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace StrikeMonster
+{
+    [RequireComponent(typeof(UnityEngine.UI.Text))]
+    public class RunTextComponent : MonoBehaviour {
+
+        public float To;
+        public float Out;
+        public float InTime = 1;
+        public float WaitTime = 0.15f;
+        public float OutTime = 0.1f;
+
+
+
+
+    	// Use this for initialization
+    	void Start () {
+           
+            SetupiTween();
+    	}
+    	
+    	// Update is called once per frame
+    	void Update () {
+    	
+    	}
+
+        private void AnimationComplete()
+        {
+            iTween.Stop(this.gameObject);
+            Destroy(this.gameObject);
+        }
+
+        private void SetupiTween()
+        {
+
+            iTween.MoveTo(this.gameObject, iTween.Hash(
+                "x", To,
+                "time", InTime,
+                "islocal", true
+                ));
+            iTween.ColorTo(this.gameObject, iTween.Hash(
+                "a", 1.0f,
+                "time", InTime,
+                "easetype", iTween.EaseType.easeOutQuad
+                ));
+
+            iTween.MoveTo(this.gameObject, iTween.Hash(
+                "x", Out,
+                "time", OutTime,
+                "delay", InTime + 0.15f,
+                "islocal", true,
+                "oncomplete","AnimationComplete",
+                "onupdatetarget", this.gameObject
+                ));
+            iTween.ColorTo(this.gameObject, iTween.Hash(
+                "a", 0f,
+                "time", OutTime,
+                "delay", InTime + 0.15f,
+                "easetype", iTween.EaseType.easeOutQuad
+                ));
+
+
+        }
+
+
+
+    }
+
+
+}
