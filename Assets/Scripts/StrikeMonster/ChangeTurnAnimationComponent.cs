2dcc3c9c52ea747469cd1cc1a9dfe18aa1524804
hongkai.chen <hongkai.chen@seedsudio.com>
2016-05-03 15:01:24 +0800

    * run text Dani

--- Assets/Scripts/StrikeMonster/ChangeTurnAnimationComponent.cs
+++ Assets/Scripts/StrikeMonster/ChangeTurnAnimationComponent.cs
@@ -1,26 +1,134 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace StrikeMonster
 {
 
     public class ChangeTurnAnimationComponent : MonoBehaviour {
 
-        public string ShowText = "1"; 
+
+        public string ShowText
+        {
+            get;
+            set;
+        }
+
+        public Color CharColor
+        {
+            get;
+            set;
+        }
+
+        [Range(0.0f, 0.3f)]
+        public float PerCharTime = 0.1f;
         public GameObject PrefabText;
 
-    	// Use this for initialization
-    	void Start () {
-    	
-    	}
-    	
-    	// Update is called once per frame
-    	void Update () {
-    	
-    	}
+        private List<RunTextComponent> m_CharList = new List<RunTextComponent>();
+
+        // Use this for initialization
+        void Start () {
+//            CreateChar();
 
 
 
+            StartCoroutine(PlayAnimation());
+        }
+        
+        // Update is called once per frame
+        void Update () {
+        
+        }
+
+        public void Play(string str, Color color)
+        {
+            CharColor = color;
+            ShowText = str;
+
+
+            StartCoroutine(PlayAnimation());
+            
+        }
+       
+
+        private void CreateChar()
+        {
+
+            char[] charArray = ShowText.ToCharArray();
+            char c;
+            m_CharList.Clear();
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                c = charArray [i];
+                
+                var runTextGo = Instantiate(PrefabText) as GameObject;
+                if (runTextGo)
+                {
+                    var runText = runTextGo.GetComponent<RunTextComponent>();
+                    if (runText)
+                    {
+                        runText.transform.SetParent(this.transform, false);
+                        runText.transform.localScale = Vector3.one;
+                        runText.Char = c;
+                        runText.CharColor = CharColor;
+
+                        m_CharList.Add(runText);
+
+                    } else
+                    {
+                        Destroy(runText);
+                        Debug.LogWarning("No has RunTextComponent");
+                        
+                    }
+                    
+                }
+            }
+
+
+
+        }
+
+
+        private IEnumerator PlayAnimation()
+        {
+            CreateChar();
+
+            yield return null;
+            yield return null;
+            
+
+            Vector3 offsetPos = Vector3.one;
+            float totalHalf = 0;
+
+            if(m_CharList.Count > 0)
+            {
+                offsetPos = m_CharList[0].transform.localPosition;
+            }
+
+            for(int i = 0; i < m_CharList.Count; i++)
+            {
+                float halfW = m_CharList[i].GetComponent<RectTransform>().sizeDelta.x / 2;
+
+                offsetPos.x += halfW;
+                m_CharList[i].transform.localPosition = offsetPos;
+                offsetPos.x += halfW;
+
+                totalHalf += halfW;
+
+//                m_CharList[i].PlayTween();
+//                yield return new WaitForSeconds(PerCharTime);
+            }
+
+            for(int i = 0; i < m_CharList.Count; i++)
+            {
+                m_CharList[i].HalfTextWidth = totalHalf;
+                m_CharList[i].PlayTween();
+                yield return new WaitForSeconds(PerCharTime);
+            }
+
+
+        }
 
 
 
--- Assets/Scripts/StrikeMonster/RunTextComponent.cs
+++ Assets/Scripts/StrikeMonster/RunTextComponent.cs
@@ -6,19 +6,43 @@ namespace StrikeMonster
     [RequireComponent(typeof(UnityEngine.UI.Text))]
     public class RunTextComponent : MonoBehaviour {
 
-        public float To;
-        public float Out;
         public float InTime = 1;
         public float WaitTime = 0.15f;
         public float OutTime = 0.1f;
 
+        public float HalfTextWidth
+        {
+            get;
+            set;
+        }
 
+        public char Char
+        {
+            set
+            {
+                this.GetComponent<UnityEngine.UI.Text>().text = value.ToString();
+            }
+        }
 
+        public Color CharColor
+        {
+            set
+            {
+                var text = this.GetComponent<UnityEngine.UI.Text>();
+                value.a = text.color.a;
+                text.color = value;
+            }
+        }
+
+        private float OutOffset = -55f;
 
     	// Use this for initialization
-    	void Start () {
-           
-            SetupiTween();
+    	void Start () {        
+            var locPos = this.transform.localPosition;
+            locPos.x = Screen.width/2;
+            this.transform.localPosition = locPos;
+
+            StartCoroutine(AdjustWidth());
     	}
     	
     	// Update is called once per frame
@@ -32,11 +56,33 @@ namespace StrikeMonster
             Destroy(this.gameObject);
         }
 
-        private void SetupiTween()
+
+        private IEnumerator AdjustWidth()
         {
+            yield return null;
+
+            var transformCmp = this.GetComponent<RectTransform>();
+            float w;
+            if (this.GetComponent<UnityEngine.UI.Text>().text != " ")
+            {
+                w = UnityEngine.UI.LayoutUtility.GetPreferredWidth(transformCmp);
+            } else
+            {
+                w = 20;
+            }
+            transformCmp.sizeDelta = new Vector2(w, transformCmp.sizeDelta.y);
+
+        }
+
+
+
+        public void PlayTween()
+        {
+            float to = this.transform.localPosition.x - Screen.width/2 - HalfTextWidth;
+
 
             iTween.MoveTo(this.gameObject, iTween.Hash(
-                "x", To,
+                "x", to,
                 "time", InTime,
                 "islocal", true
                 ));
@@ -47,9 +93,9 @@ namespace StrikeMonster
                 ));
 
             iTween.MoveTo(this.gameObject, iTween.Hash(
-                "x", Out,
+                "x", to + OutOffset,
                 "time", OutTime,
-                "delay", InTime + 0.15f,
+                "delay", InTime + WaitTime,
                 "islocal", true,
                 "oncomplete","AnimationComplete",
                 "onupdatetarget", this.gameObject
@@ -57,7 +103,7 @@ namespace StrikeMonster
             iTween.ColorTo(this.gameObject, iTween.Hash(
                 "a", 0f,
                 "time", OutTime,
-                "delay", InTime + 0.15f,
+                "delay", InTime + WaitTime,
                 "easetype", iTween.EaseType.easeOutQuad
                 ));
 
